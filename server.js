2303e98208a292989e5829c11e7fdd2877fe7cc0
Isabella Torres <izzy612@gmail.com>
2019-05-08 01:46:14 -0400

    theres a bug

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require("express");
 var connection = require('./db/connection');
 var path = require("path");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "class",
+  database: "noted_db"
+});
+
+
+
 
 
 var app = express();
@@ -18,7 +30,7 @@ app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "./html/notes.html"));
 });
 
-app.get("./api/notes", function (req, res) {
+app.get("/api/notes", function (req, res) {
   connection.query("SELECT * FROM tables", function (err, data) {
     if (err) {
       return res.json(err);
@@ -27,6 +39,27 @@ app.get("./api/notes", function (req, res) {
     
   });
 });
+
+app.post("/api/notes", function (req, res) {
+  connection.query("INSERT INTO tables SET ?",
+    req.txt, function (err, res) {
+      if (err) {
+        return res.json(err);
+      }
+      else {
+        console.log(res);
+      }
+    
+    });
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  connection.query("DELETE FROM tables where id = ?", [req.params.id], function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
 
 app.listen(PORT, function () {
   console.log("App is listening on PORT: " + PORT);
